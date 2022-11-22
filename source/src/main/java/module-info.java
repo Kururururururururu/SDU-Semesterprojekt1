ab0267206d4d18b0e3d71aff4a0bd10757aa2ab3
Marcus Ellested <marcusellested02@gmail.com>
2022-11-22 09:18:01 +0100

    David made me do it

---
+++ source/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.greenwatts {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.greenwatts to javafx.fxml;
+    exports com.example.greenwatts;
+}
