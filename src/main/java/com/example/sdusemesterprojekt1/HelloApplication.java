b44e08f8e41f47798d2759a99ce72a16b25f3a0c
jonaslissner <jolis21@student.sdu.dk>
2022-11-27 09:59:21 +0100

    Made buttons clickable for all scenes

--- src/main/java/com/example/sdusemesterprojekt1/HelloApplication.java
+++ src/main/java/com/example/sdusemesterprojekt1/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        game.showScene("coast");
+        game.showScene("hub");
     }
 
 
