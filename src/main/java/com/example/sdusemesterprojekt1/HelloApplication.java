eaf683d526e27f36476aff7678488d68453d4588
jonaslissner <jolis21@student.sdu.dk>
2022-11-27 12:43:06 +0100

    Update HelloApplication.java

--- src/main/java/com/example/sdusemesterprojekt1/HelloApplication.java
+++ src/main/java/com/example/sdusemesterprojekt1/HelloApplication.java
@@ -19,7 +19,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        game.showScene("hub");
+        game.showScene("solarcity");
     }
 
 
